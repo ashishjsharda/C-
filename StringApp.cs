62d0c3b6e5e2fbc593a6327a9145f3cbc1a14663
Ashish S <ashishjsharda@gmail.com>
2020-01-11 10:01:38 -0500

    Create StringApp.cs

---
+++ StringApp.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace StringDemo
+{
+	class StringApp
+	{
+
+		static void Main(string[] args)
+		{
+			string strA = "Sai";
+			string strB = "Ram";
+			string output = strA + strB;
+			Console.WriteLine("Combined String is {0}", output);
+			Console.ReadKey();
+		}
+	}
+
+}
