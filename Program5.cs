4458073846bfa7e412adde5980e836ec5b6753e0
Ashish S <ashishjsharda@gmail.com>
2020-05-17 09:49:55 -0400

    Create Program5.cs

---
+++ Program5.cs
@@ -0,0 +1,16 @@
+using System;
+
+    class Program5
+    {
+   
+        static void Main(string[] args)
+        {
+
+        int? num1 = null;
+        int num2 = 20;
+        Console.WriteLine("Value of num1 is {0}", num1);
+        Console.WriteLine("Value of num2 is {0}", num2);
+
+    }
+    }
+
