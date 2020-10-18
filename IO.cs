1ef7929a28e884c8b9cf71a96f635b4a80dce5b6
Ashish S <ashishjsharda@gmail.com>
2020-10-18 07:46:49 -0400

    Create IO.cs

---
+++ IO.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace IoDemo
+{
+    class IO
+    {
+        
+        static void Main(string[] args)
+        {
+            String firstName;
+            Console.Write("Enter first Name");
+            firstName=Console.ReadLine();
+            Console.WriteLine(firstName);
+        }
+    }
+
+    
+}
